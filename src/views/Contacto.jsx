ab73b025d743a2f9a0b23f950fbef3dc8b290383
Carlos Velasquez <carlos.velasquez.sandoval@gmail.com>
2022-11-27 18:47:34 -0300

    primer commit

---
+++ src/views/Contacto.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+import Contacto from "../components/Contacto"
+
+export default () => {
+    return(
+        <Container>
+            <h2 className="text-center">Cuentanos, ¿En que podemos ayudar?</h2>
+            <Contacto />
+        </Container>
+    )
+}
+
